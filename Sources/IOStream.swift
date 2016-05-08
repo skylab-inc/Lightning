80ccb576f0f7f02fc40ccf0c86adbb3749aacf96
Tyler Cloutier <cloutiertyler@aol.com>
2016-05-08 03:21:30 -0700

    Fixed le bugs.

--- Sources/IOStream.swift
+++ Sources/IOStream.swift
@@ -32,7 +32,10 @@ public extension IOStream {
     public func read(minBytes: Int = 1, onRead: (result: Result<UnsafeBufferPointer<Int8>, Error>) -> ()) {
         dispatch_io_set_low_water(channel, minBytes);
         dispatch_io_read(channel, off_t(), size_t(INT_MAX), dispatch_get_main_queue()) { done, data, error in
-            onRead(result: Result(error: Error(rawValue: error)))
+            if error != 0 {
+                onRead(result: Result(error: Error(rawValue: error)))
+                return
+            }
             var p = UnsafePointer<Void>(nil)
             var size: size_t = 0
             _ = dispatch_data_create_map(data, &p, &size)
@@ -44,7 +47,11 @@ public extension IOStream {
     public func write(buffer: UnsafeBufferPointer<Int8>, onWrite: ((result: Result<Void, Error>) -> ())? = nil) {
         let dispatchData = dispatch_data_create(buffer.baseAddress, buffer.count, dispatch_get_main_queue(), nil)
         dispatch_io_write(channel, off_t(), dispatchData, dispatch_get_main_queue()) { done, data, error in
-            onWrite?(result: Result(error: Error(rawValue: error)))
+            if error != 0 {
+                onWrite?(result: Result(error: Error(rawValue: error)))
+            } else {
+                onWrite?(result: Result(value: ()))
+            }
         }
     }
 }
