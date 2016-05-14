d0543178b7d31a987e4bc453e0937a31c33bd0a3
Tyler Cloutier <cloutiertyler@aol.com>
2016-05-14 16:15:49 -0700

    Added additional result functionality.

--- Sources/Result.swift
+++ Sources/Result.swift
@@ -6,7 +6,7 @@
 //
 //
 
-public enum Result<T, Error: ErrorProtocol>: CustomStringConvertible, CustomDebugStringConvertible {
+public enum Result<T, Error: ErrorProtocol>: ResultType, CustomStringConvertible, CustomDebugStringConvertible {
     case Success(T)
     case Failure(Error)
     
@@ -42,4 +42,45 @@ public enum Result<T, Error: ErrorProtocol>: CustomStringConvertible, CustomDebu
     public var debugDescription: String {
         return description
     }
+}
+
+public extension ResultType {
+    
+    /// Returns the value if self represents a success, `nil` otherwise.
+    public var value: Value? {
+        return analysis(ifSuccess: { $0 }, ifFailure: { _ in nil })
+    }
+    
+    /// Returns the error if self represents a failure, `nil` otherwise.
+    public var error: Error? {
+        return analysis(ifSuccess: { _ in nil }, ifFailure: { $0 })
+    }
+    
+}
+
+/// A type that can represent either failure with an error or success with a result value.
+public protocol ResultType {
+    associatedtype Value
+    associatedtype Error: ErrorProtocol
+    
+    /// Constructs a successful result wrapping a `value`.
+    init(value: Value)
+    
+    /// Constructs a failed result wrapping an `error`.
+    init(error: Error)
+    
+    /// Case analysis for ResultType.
+    ///
+    /// Returns the value produced by appliying `ifFailure` to the error if self represents a failure, or `ifSuccess` to the result value if self represents a success.
+    func analysis<U>(ifSuccess: @noescape (Value) -> U, ifFailure: @noescape (Error) -> U) -> U
+    
+    /// Returns the value if self represents a success, `nil` otherwise.
+    ///
+    /// A default implementation is provided by a protocol extension. Conforming types may specialize it.
+    var value: Value? { get }
+    
+    /// Returns the error if self represents a failure, `nil` otherwise.
+    ///
+    /// A default implementation is provided by a protocol extension. Conforming types may specialize it.
+    var error: Error? { get }
 }
