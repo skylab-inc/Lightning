4eddfec8e7028d9cb0b1b17f58738327ff8fac39
Tyler Cloutier <cloutiertyler@aol.com>
2016-06-19 23:05:04 -0700

    Changed project, updated gitignore, and added basic test for TCP.

---
+++ Tests/TCP/Tests.swift
@@ -0,0 +1,58 @@
+@testable import TCP
+import XCTest
+
+class TestBasic: XCTestCase {
+
+    func testClientServer() {
+
+        let interruptExpectation = expectation(withDescription: "readInterrupt")
+        let completeWriteExpectation = expectation(withDescription: "completeWrite")
+        let server = try! Server()
+        try! server.bind(host: "localhost", port: 50000)
+
+        server.listen().startWithNext { connection in
+            let read = connection.read()
+            let strings = read.map { String(bytes: $0, encoding: .utf8)! }
+
+            strings.onNext { message in
+                XCTAssert(message == "This is a test", "Incorrect message.")
+                read.stop()
+            }
+
+            strings.onInterrupted {
+                interruptExpectation.fulfill()
+            }
+
+            strings.onFailed { error in
+                XCTFail("Read failed with error: \(error)")
+            }
+
+            strings.onCompleted {
+                XCTFail("Completed instead of interrupt.")
+            }
+            read.start()
+        }
+
+        let socket = try! Socket()
+        let connect = socket.connect(host: "localhost", port: 50000)
+        connect.onCompleted {
+            let buffer = Array("This is a test".utf8)
+            let write = socket.write(buffer: buffer)
+            write.onCompleted {
+                completeWriteExpectation.fulfill()
+                connect.stop()
+            }
+            write.onFailed { error in
+                XCTFail("Write failed with error: \(error)")
+            }
+            write.start()
+        }
+        connect.onFailed { error in
+            XCTFail("Connection failed with error: \(error)")
+        }
+        connect.start()
+
+        waitForExpectations(withTimeout: 1) { _ in }
+    }
+
+}
