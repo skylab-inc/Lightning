303bafe94c3537c5ffba67c6ccea96ef5c4c5a2b
Tyler Cloutier <cloutiertyler@aol.com>
2016-06-29 10:38:18 -0700

    Added HTTP parsing to the server.

--- Sources/HTTP/Server.swift
+++ Sources/HTTP/Server.swift
@@ -14,40 +14,36 @@ import TCP
 
 public final class Server {
     
-    public let parser: RequestParser
+    private var observer: Observer<Request, SystemError>! = nil
     
     init() {
-        self.parser = RequestParser()
+
     }
     
     public func listen(host: String, port: Port) -> ColdSignal<Request, SystemError> {
         return ColdSignal { observer in
+            self.observer = observer
             
             let tcpServer = try! TCP.Server()
             try! tcpServer.bind(host: host, port: port)
             
             let listen = tcpServer.listen()
             listen.onNext { connection in
-                /*
+                
+                // Create parser for connection/
+                let parser = RequestParser { request in
+                    self.observer.sendNext(request)
+                }
+                
+                // Read from connection and parse data.
                 let read = connection.read()
                 read.onNext { data in
-                    if let request = try! self.parser.parse(Data(data)) {
-                        let response = try! middleware.chain(to: responder).respond(to: request)
-                        try! serializer.serialize(response, to: stream)
-
-                        if let upgrade = response.didUpgrade {
-                            try! upgrade(request, stream)
-                            try! stream.close()
-                        }
-
-                        if !request.isKeepAlive {
-                            try! stream.close()
-                        }
-                    }
-                    
+                    try! parser.parse(data)
                 }
                 read.start()
-                */
+            }
+            listen.onFailed { error in
+                observer.sendFailed(error)
             }
             listen.start()
 
