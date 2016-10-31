fe18a12a2c33bc3d1d00375c7d7ead85d7d0f2ac
Tyler Cloutier <cloutiertyler@aol.com>
2016-10-30 17:39:45 -0700

    Updated code-coverage.sh to only run on macOS

--- Scripts/TravisCI/code-coverage.sh
+++ Scripts/TravisCI/code-coverage.sh
@@ -2,4 +2,27 @@
 
 set -euf -o pipefail
 
+UNAME=`uname`;
+
+if [[ $UNAME != "Darwin" ]];
+then
+    echo "Code coverage for $UNAME is not yet supported. Skip coverage."
+    exit 0;
+fi
+
+GENERATOR_OUTPUT=`swift package generate-xcodeproj`;
+PROJECT_NAME="${GENERATOR_OUTPUT/generated: .\//}";
+SCHEME_NAME="${PROJECT_NAME/.xcodeproj/}";
+
+rvm install 2.2.3
+gem install xcpretty
+WORKING_DIR=$(PWD) xcodebuild \
+    -project $PROJECT_NAME \
+    -scheme $SCHEME_NAME \
+    -sdk macosx10.12 \
+    -destination arch=x86_64 \
+    -configuration Debug \
+    -enableCodeCoverage YES \
+    test | xcpretty
+
 bash <(curl -s https://codecov.io/bash)
